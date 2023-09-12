c3db4a47d782c64aa3c63eca03b57b22f8a2873b
alban <git@albancrommer.com>
2023-09-12 15:47:33 +0200

    fix: use secgroup by id and not name

--- 04-ovh-cloud/_common/56-instance-bastion.tf
+++ 04-ovh-cloud/_common/56-instance-bastion.tf
@@ -4,7 +4,7 @@ resource "openstack_compute_instance_v2" "certification_bastion" {
   image_id    = data.openstack_images_image_v2.some_distro.id
   flavor_name = "d2-2"
   key_pair    = openstack_compute_keypair_v2.certification_key.id
-  security_groups = [openstack_networking_secgroup_v2.certification_secgroup.name]
+  security_groups = [openstack_networking_secgroup_v2.certification_secgroup.id]
   network {
     uuid = data.openstack_networking_network_v2.extnet.id
   }
