b60d55e49aabf35bc8f49a1b34775201eda6ab1e
alban <git@albancrommer.com>
2023-09-13 13:35:03 +0200

    feat: scaleway modules

---
+++ 04-scaleway/02-module/00-variables.tf
@@ -0,0 +1,20 @@
+variable "scaleway_api_secret_key" {
+  type      = string
+  default   = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+  sensitive = true
+}
+variable "scaleway_api_access_key" {
+  type      = string
+  default   = "SCW00000000000000000"
+  sensitive = true
+}
+variable "scaleway_project_id" {
+  type      = string
+  default   = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+  sensitive = true
+}
+variable "scaleway_organization_id" {
+  type      = string
+  default   = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+  sensitive = true
+}
