1f37cb9c5c7e3feb38a2af110f0e77b50fbb73da
Elie Gavoty <eliegavoty@free.fr>
2025-07-02 23:36:34 +0200

    improve iam policy for AMI creation using packer

--- provision_iam_locally/main.tf
+++ provision_iam_locally/main.tf
@@ -26,7 +26,7 @@ resource "aws_iam_access_key" "stagiaires" {
   user     = each.value.name
 }
 
-# Politique pour les stagiaires
+# Politique pour les développeurs
 resource "aws_iam_policy" "stagiaires_policy" {
   name        = "StagiairesPolicy"
   description = "Policy for stagiaires with S3, EC2, and AMI permissions"
@@ -34,141 +34,69 @@ resource "aws_iam_policy" "stagiaires_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # S3 Permissions
       {
-        Sid    = "S3BucketManagement"
-        Effect = "Allow"
-        Action = [
-          "s3:CreateBucket",
-          "s3:DeleteBucket",
-          "s3:ListBucket",
-          "s3:GetBucketLocation",
-          "s3:GetBucketVersioning",
-          "s3:PutBucketVersioning",
-          "s3:GetBucketTagging",
-          "s3:PutBucketTagging"
-        ]
-        Resource = "arn:aws:s3:::*"
-      },
-      {
-        Sid    = "S3ObjectManagement"
         Effect = "Allow"
         Action = [
           "s3:GetObject",
           "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:GetObjectVersion",
-          "s3:DeleteObjectVersion"
+          "s3:ListBucket"
         ]
-        Resource = "arn:aws:s3:::*/*"
+        Resource = [
+          "arn:aws:s3:::dev-bucket/*",
+          "arn:aws:s3:::dev-bucket"
+        ]
       },
       {
-        Sid    = "S3ListAllBuckets"
         Effect = "Allow"
         Action = [
-          "s3:ListAllMyBuckets",
-          "s3:GetBucketPolicy",
-          "s3:PutBucketPolicy",
-          "s3:DeleteBucketPolicy"
+          "s3:ListAllMyBuckets"
         ]
         Resource = "*"
       },
-      # EC2 Permissions
       {
-        Sid    = "EC2InstanceManagement"
         Effect = "Allow"
         Action = [
-          "ec2:RunInstances",
-          "ec2:TerminateInstances",
-          "ec2:StartInstances",
-          "ec2:StopInstances",
-          "ec2:RebootInstances",
+          "ec2:DescribeRegions",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeImages",
           "ec2:DescribeInstances",
           "ec2:DescribeInstanceStatus",
-          "ec2:DescribeInstanceAttribute",
-          "ec2:DescribeInstanceTypes",
-          "ec2:DescribeInstanceTypeOfferings",
-          "ec2:DescribeInstanceCreditSpecifications",
-          "ec2:ModifyInstanceCreditSpecification",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeVolumes",
+          "ec2:DetachVolume",
+          "ec2:AttachVolume",
           "ec2:ModifyInstanceAttribute",
+          "ec2:RunInstances",
+          "ec2:StopInstances",
+          "ec2:RebootInstances",
+          "ec2:TerminateInstances",
+          "ec2:CreateImage",
+          "ec2:RegisterImage",
+          "ec2:CreateSnapshot",
+          "ec2:DeleteSnapshot",
           "ec2:CreateTags",
-          "ec2:DeleteTags",
-          "ec2:DescribeTags"
-        ]
-        Resource = "*"
-      },
-      {
-        Sid    = "EC2SecurityGroupManagement"
-        Effect = "Allow"
-        Action = [
-          "ec2:CreateSecurityGroup",
-          "ec2:DeleteSecurityGroup",
+          "ec2:DescribeTags",
+          "ec2:GetPasswordData",
           "ec2:DescribeSecurityGroups",
           "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:AuthorizeSecurityGroupEgress",
           "ec2:RevokeSecurityGroupIngress",
-          "ec2:RevokeSecurityGroupEgress"
-        ]
-        Resource = "*"
-      },
-      {
-        Sid    = "EC2KeyPairManagement"
-        Effect = "Allow"
-        Action = [
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeKeyPairs",
           "ec2:CreateKeyPair",
           "ec2:DeleteKeyPair",
-          "ec2:DescribeKeyPairs",
           "ec2:ImportKeyPair"
         ]
         Resource = "*"
       },
-      # AMI Permissions
       {
-        Sid    = "AMIAccess"
         Effect = "Allow"
         Action = [
-          "ec2:DescribeImages",
-          "ec2:DescribeImageAttribute",
-          "ec2:DescribeSnapshots",
-          "ec2:DescribeSnapshotAttribute"
+          "iam:PassRole"
         ]
-        Resource = "*"
-      },
-      # VPC and Networking (needed for EC2)
-      {
-        Sid    = "VPCNetworking"
-        Effect = "Allow"
-        Action = [
-          "ec2:DescribeVpcs",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DescribeAvailabilityZones",
-          "ec2:DescribeInternetGateways",
-          "ec2:DescribeRouteTables",
-          "ec2:AllocateAddress",
-          "ec2:ReleaseAddress",
-          "ec2:AssociateAddress",
-          "ec2:DisassociateAddress",
-          "ec2:DescribeAddresses"
-        ]
-        Resource = "*"
-      },
-      # EBS Volumes (needed for EC2)
-      {
-        Sid    = "EBSVolumeManagement"
-        Effect = "Allow"
-        Action = [
-          "ec2:CreateVolume",
-          "ec2:DeleteVolume",
-          "ec2:DescribeVolumes",
-          "ec2:AttachVolume",
-          "ec2:DetachVolume",
-          "ec2:ModifyVolumeAttribute",
-          "ec2:DescribeVolumeAttribute",
-          "ec2:CreateSnapshot",
-          "ec2:DeleteSnapshot"
-        ]
-        Resource = "*"
+        Resource = "arn:aws:iam::*:role/packer*"
       }
     ]
   })
@@ -181,70 +109,6 @@ resource "aws_iam_user_policy_attachment" "stagiaires" {
   policy_arn = aws_iam_policy.stagiaires_policy.arn
 }
 
-# Get the first user name
-locals {
-  first_user            = length(var.user_names) > 0 ? var.user_names[0] : ""
-  first_user_access_key = local.first_user != "" ? aws_iam_access_key.stagiaires[local.first_user] : null
-}
-
-# Manage AWS CLI profile using AWS CLI commands
-resource "null_resource" "aws_cli_profile" {
-  count = local.first_user != "" && var.manage_aws_cli_config ? 1 : 0
-
-  depends_on = [aws_iam_access_key.stagiaires]
-
-  triggers = {
-    profile_name = local.first_user
-    access_key   = local.first_user_access_key.id
-  }
-
-  # Create or update the AWS CLI profile
-  provisioner "local-exec" {
-    command = <<-EOT
-      # Check if AWS CLI is installed
-      if ! command -v aws &> /dev/null; then
-        echo "AWS CLI is not installed. Please install it to manage profiles."
-        exit 1
-      fi
-
-      # Configure the profile
-      aws configure set aws_access_key_id ${local.first_user_access_key.id} --profile ${local.first_user}
-      aws configure set aws_secret_access_key ${local.first_user_access_key.secret} --profile ${local.first_user}
-      aws configure set region ${var.aws_region} --profile ${local.first_user}
-      aws configure set output ${var.aws_cli_output_format} --profile ${local.first_user}
-      
-      echo "AWS CLI profile '${local.first_user}' has been configured successfully."
-    EOT
-  }
-
-  # Remove the profile on destroy
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<-EOT
-      # Check if AWS CLI is installed
-      if ! command -v aws &> /dev/null; then
-        echo "AWS CLI is not installed. Skipping profile removal."
-        exit 0
-      fi
-
-      # Remove the profile entries
-      profile_name="${self.triggers.profile_name}"
-      
-      # Remove from credentials file
-      if [ -f ~/.aws/credentials ]; then
-        sed -i.bak "/^\[$${profile_name}\]/,/^\[/{ /^\[$${profile_name}\]/d; /^\[/!d; }" ~/.aws/credentials
-      fi
-      
-      # Remove from config file
-      if [ -f ~/.aws/config ]; then
-        sed -i.bak "/^\[profile $${profile_name}\]/,/^\[/{ /^\[profile $${profile_name}\]/d; /^\[/!d; }" ~/.aws/config
-      fi
-      
-      echo "AWS CLI profile '$${profile_name}' has been removed."
-    EOT
-  }
-}
-
 # Outputs pour récupérer les informations
 output "user_access_keys" {
   value = {
@@ -254,14 +118,4 @@ output "user_access_keys" {
     }
   }
   sensitive = true
-}
-
-output "first_user_profile" {
-  value = local.first_user != "" ? {
-    profile_name  = local.first_user
-    access_key    = local.first_user_access_key.id
-    usage_example = "aws s3 ls --profile ${local.first_user}"
-  } : null
-  sensitive   = true
-  description = "AWS CLI profile created for the first user"
 }
