3d1b268c0912500420bed5b5f520996aed79f141
Elie Gavoty <eliegavoty@free.fr>
2025-07-02 17:43:42 +0200

    format TP 31

--- 31-Modules/aws/modules/autoscaling/main.tf
+++ 31-Modules/aws/modules/autoscaling/main.tf
@@ -21,13 +21,13 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-  resource "aws_launch_template" "webserver" {
+resource "aws_launch_template" "webserver" {
   name_prefix   = var.namespace
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   # see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html#user-data-cloud-init
-  user_data     = data.cloudinit_config.config.rendered
-  key_name      = var.ssh_keypair
+  user_data = data.cloudinit_config.config.rendered
+  key_name  = var.ssh_keypair
   iam_instance_profile {
     name = module.iam_instance_profile.name
   }
